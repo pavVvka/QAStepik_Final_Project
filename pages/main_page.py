77b2caddcb374b205738dc3ff5b05de2428eca1c
Павел <pavVvka@gmail.com>
2021-12-29 19:43:47 +0300

    added assert: "Login link is absent"

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,3 +7,7 @@ class MainPage(BasePage):
         # login_link = browser.find_element_by_css_selector("#login_link")
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        # self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is absent"
