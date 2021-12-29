4c46719ba93bc0356542b990aa563108cbc8e1f8
Павел <pavVvka@gmail.com>
2021-12-29 18:52:10 +0300

    added method: go_to_login_page()

--- test_main_page.py
+++ test_main_page.py
@@ -3,15 +3,21 @@
 #     browser.get(link)
 #     login_link = browser.find_element_by_css_selector("#login_link")
 #     login_link.click()
-
-link = "http://selenium1py.pythonanywhere.com/"
-
-
-def go_to_login_page(browser):
-    link = browser.find_element_by_css_selector("#login_link")
-    link.click()
+from .pages.main_page import MainPage
+# link = "http://selenium1py.pythonanywhere.com/"
+#
+#
+# def go_to_login_page(browser):
+#     link = browser.find_element_by_css_selector("#login_link")
+#     link.click()
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    # browser.get(link)
+    # go_to_login_page(browser)
+
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
+    page.open()
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+
