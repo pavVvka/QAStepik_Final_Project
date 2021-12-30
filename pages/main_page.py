0aeb934b9cc9ddf0fa9bb20feacd17e78cd44081
Павел <pavVvka@gmail.com>
2021-12-30 17:57:46 +0300

    added assertions

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,16 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         # login_link = browser.find_element_by_css_selector("#login_link")
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        # login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        # login_link.click()
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
     def should_be_login_link(self):
         # self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is absent"
+        # assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is absent"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
